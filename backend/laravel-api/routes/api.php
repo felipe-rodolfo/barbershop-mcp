f8e1ad5b130b3c78dd65ccf12f2c776736e7c8d6
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2026-06-09 20:16:06 -0300

    feat: create API routes for v1 endpoints

--- backend/laravel-api/routes/api.php
+++ backend/laravel-api/routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\BarbershopController;
+use App\Http\Controllers\Api\HaircutController;
+use App\Http\Controllers\Api\TimeSlotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::get('/barbershops', [BarbershopController::class, 'index']);
+    Route::get('/barbershops/{id}', [BarbershopController::class, 'show']);
+
+    Route::get('/haircuts', [HaircutController::class, 'index']);
+    Route::get('/haircuts/{id}', [HaircutController::class, 'show']);
+
+    Route::get('/timeslots', [TimeSlotController::class, 'index']);
+    Route::get('/timeslots/{id}', [TimeSlotController::class, 'show']);
+
+    Route::get('/appointments', [AppointmentController::class, 'index']);
+    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::get('/appointments/{id}', [AppointmentController::class, 'show']);
+});
